f731849be210e3b9367eaec2518072c367cc2ccf
umarkhan <ku6205939@gmail.com>
2024-04-30 15:11:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Variable Task
+//Describe two Variables:admin and name
+let admin;
+let name;
+//Assign the value "john" to name
+name = "John";
+//copy the value of admin from name to admin
+admin = name;
+//show the value of admin using allert
+console.log(admin); //there will bw out put "John"
---
+++ index.ts
@@ -0,0 +1,11 @@
+//Variable Task
+//Describe two Variables:admin and name
+let admin;
+let name;
+
+//Assign the value "john" to name
+name = "John";
+//copy the value of admin from name to admin
+admin = name;
+//show the value of admin using allert
+console.log(admin);//there will bw out put "John"
